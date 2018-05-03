3e10c2e815d7f504cace05f835a29a24e70d0652
StephenEllsworth <spegoog@gmail.com>
2018-05-03 13:07:07 -0500

    lvl

--- src/Level.java
+++ src/Level.java
@@ -12,24 +12,21 @@ public class Level extends World {
     private BuildingHandler bh;
 
     public Level() {
-        map = new CellMap(54,54);
-        for(int i = 0; i < map.rows(); i++)
-        {
-            for(int j = 0; j < map.cols(); j++)
-            {
-                Grass g = new Grass(i*50,j*50,50,50);
-                addObject(g,i*50,j*50);
-                map.setCell(i,j,g);
+        map = new CellMap(54, 54);
+        for (int i = 0; i < map.rows(); i++) {
+            for (int j = 0; j < map.cols(); j++) {
+                Grass g = new Grass(i * 50, j * 50, 50, 50);
+                addObject(g, i * 50, j * 50);
+                map.setCell(i, j, g);
             }
         }
 
         bh = new BuildingHandler();
-        addObject(bh,0,0);
+        addObject(bh, 0, 0);
     }
 
-    public void act()
-    {
-        if(bh.updateMap() != null)
+    public void act() {
+        if (bh.updateMap() != null)
             map = bh.updateMap();
 
         /*MouseInfo mouse = Mayflower.getMouseInfo();
@@ -54,8 +51,3 @@ public class Level extends World {
         return map;
     }
 }
-
-    public void moveTo(Object road, Object car, int xCord, int yCord)
-    {
-
-    }
